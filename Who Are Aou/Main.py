9e944c87d277959721debfc227e8b56d91976664
AaronWang7 <aaron.wang@ucas-edu.net>
2024-09-24 10:37:45 -0600

    AW-CP1 Projects

---
+++ Who Are Aou/Main.py
@@ -0,0 +1,8 @@
+#Aaron Wang, Who Are you
+
+name = input("What's your name?")
+age = input("what's your age?")
+Color = input("waht's your favorite color?")
+
+print("hi",name,"nice to meet you, you are" ,age,"years old ,", "And your favorite color is",Color)
+
---
+++ main.py
@@ -0,0 +1,15 @@
+i = 1
+
+for i in range (1,10,0):
+
+ print(i+1)
+
+
+
+
+
+#i = 1
+#while True (i <= 10):
+
+#    print( i + 1 )
+    
