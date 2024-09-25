e41ef1597e3f130bc90940b866ca811de5db13db
AaronWang7 <aaron.wang@ucas-edu.net>
2024-09-25 10:46:40 -0600

    Aaron

---
+++ Madlib/Main.py
@@ -0,0 +1,7 @@
+first = input("fill in the blank,action word (blank) one word only: ")
+nd = input("fill in the blank,object, (blank): ")
+rd = input("fill in the blank,action word ,(blank)")
+fo  = input("fill in the blank,action word , (blank): ")
+fiv = input("fill in the blank,action word , (blank): ")
+
+print ("Here's the story you just made,toay I wake up and  ",first,"then I wnet to school, and I saw a",nd, "then I wnet to Pe class, I saw my classmate",rd,"After Pe class I went to Compuer class, and I saw the computer is",fo,",After school I wnet back home, and I saw a cat",fiv)
