f326075fa9a53e7ed6fcdfe2b4fd75594103f593
AaronWang7 <aaron.wang@ucas-edu.net>
2024-10-01 10:42:54 -0600

    simple c

--- personal info/Main.py
+++ personal info/Main.py
@@ -4,8 +4,6 @@ age = input("what's your age?:")
 h = input ("How tall are you?, in meters:")
 number = input("What's your fav number?")
 
-
-
 print("name,",na,"age",age,"hight",h,"fav number,",number)
 print (int(age))
 print (int(number))
