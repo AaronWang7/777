e0bb8af16e2b8a3ff1c95901b96b966bb9129c2b
AaronWang7 <aaron.wang@ucas-edu.net>
2024-10-15 10:19:45 -0600

    hello name

---
+++ comments challenge/Main.py
@@ -0,0 +1,6 @@
+"""
+This is a code that takes in user's name as input
+and out put hello{name}
+"""
+inp = input("Your name:") #takes in the name as input
+print("hello",inp)# output the name and add hello.
