17553798bd497a9083a298c4ad9653010e256f96
AaronWang7 <aaron.wang@ucas-edu.net>
2024-11-20 10:20:21 -0700

    This is a rock paper scissors Python code

--- rockpaperscissors/Main.py
+++ rockpaperscissors/Main.py
@@ -1,3 +1,4 @@
+score = []
 while True:
        import random
        player = input("Enter one of the numbers, 1 = rock, 2 = paper, 3 = scissors:")
@@ -10,22 +11,33 @@ while True:
               aii = ("scissors")
        if player == "1" and aii == "rock":
               print(f"tie, you both used ,{aii}")
+              print(score)
+              
        elif player == "1" and aii == ("paper"):
               print(f"Oh no you lost,you used,{player},and ai used,{aii}")
+              print(score)
        elif player == "1" and aii == ("scissors"):
               print(f"You won!,you used,{player},and ai used,{aii}")
-
+              score.append("I")
+              print(score)
        if player == "2" and aii == "rock":
               print(f"You won!, you used ,{player}, ai used ,{aii}")
+              score.append("I")
+              print(score)
        elif player == "2" and aii == ("paper"):
-              print(f"tie you both used {player}")
+              print(f"tie you both used {aii}")
+              print(score)
        elif player == "2" and aii == ("scissors"):
               print(f"Oh no you lost,you used,{player},and ai used,{aii}")
+              print(score)
 
        elif player == "3" and aii == "rock":
               print(f"Oh no you lost,you used,{player},and ai used,{aii}")
+              print(score)
        elif player == "3" and aii == ("paper"):
               print(f"You won!,you used,{player},and ai used,{aii}")
+              score.append("I")
+              print(score)
        elif player == "3" and aii == ("scissors"):
               print(f"tie, you both used ,{aii}")
-
+              print(score)
