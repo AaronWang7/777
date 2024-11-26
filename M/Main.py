8af5b08453ca11a962aa9ea33d98355d712caec7
AaronWang7 <aaron.wang@ucas-edu.net>
2024-11-26 10:13:54 -0700

    m

--- M/Main.py
+++ M/Main.py
@@ -1,10 +1,12 @@
-try:
- 
-    number = int(input("Enter the number you want multiples of: "))
+while True:
+    try:
+    
+        number = int(input("Enter the number you want multiples of: "))
 
-    print(f"Multiples of {number} from 0 to 12:")
-    for i in range(1,13,1):
-        print(f"{number} x {i} = {number * i}")
-except ValueError:
-    print("Error: Please enter a valid number.")
+        print(f"This is your multiples of {number}, from 0 to 12:")
+        for i in range(1,13,1):
+            print(f"{number} x {i} = {number * i}")
+    except ValueError:
+        print("Error: Please enter a valid number, example : 1,2,3,4,5,6,7,8,9,10,11,12.")
+        print("Now try again!")
 
