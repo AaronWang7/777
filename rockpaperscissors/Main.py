63318d4565759a7ed2181ccf6111867b7ed50b2e
AaronWang7 <aaron.wang@ucas-edu.net>
2024-11-26 10:18:36 -0700

    this will work

--- rockpaperscissors/Main.py
+++ rockpaperscissors/Main.py
@@ -6,7 +6,8 @@ while True:
        player = input("Enter one of the numbers, 1 = rock, 2 = paper, 3 = scissors:")
        if player not in op:
               print("Enter 1,2, or 3!")
-              break
+              player = input("Enter one of the numbers, 1 = rock, 2 = paper, 3 = scissors:")
+              
        else:
               ai = (random.randrange(0,60))
        if ai <= 20:
@@ -51,6 +52,11 @@ while True:
               print("This is your score, each'I'= 1 point",score)
               print("  This is ai score, each'I'= 1 point",aiscore)
        elif player == "3" and aii == ("paper"):
+              print(f"You won!,you used,{player},and ai used,{aii}")
+              score.append("I")
+              print("This is your score, each'I'= 1 point",score)
+              print("  This is ai score, each'I'= 1 point",aiscore)
+       elif player == "3" and aii == ("scissors"):
               print(f"You won!,you used,{player},and ai used,{aii}")
               score.append("I")
               print("This is your score, each'I'= 1 point",score)
